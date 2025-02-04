4e68e8066066cbe4b05c627f7bb3628be5a6d645
Douglas Hugo <douglas.hugo@live.com>
2025-02-03 22:45:15 -0300

    api livros

---
+++ basic-express/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        nome: "Harry Potter",
+    },
+    {
+        id: 2,
+        nome: "O Hobbit"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send('Curso de Express')
+});
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+});
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso!!!')
+});
+
+export default app;
