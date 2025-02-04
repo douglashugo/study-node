e57332ae2a0b519cefc233a70fe6cd4e6c0e4fb5
Douglas Hugo <douglas.hugo@live.com>
2025-02-04 17:55:51 -0300

    get and update book

--- basic-express/src/app.js
+++ basic-express/src/app.js
@@ -14,15 +14,39 @@ const livros = [
     }
 ]
 
+function buscarLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send('Curso de Express')
 });
+
 app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 });
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscarLivro(req.params.id)
+    res.status(201).json(livros[index])
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso!!!')
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscarLivro(req.params.id)
+    if (index != -1) {
+        livros[index].nome = req.body.nome
+        res.status(200).json({message: 'Livro atualizado com sucesso', livro: livros[index]});
+    } else {
+        res.status(404).json({message: 'Livro não encontrado'})
+    }
+    
 });
 
 export default app;
