ce14fadb34af566117096f3d7647bd7a2e2d46ab
Douglas Hugo <douglas.hugo@live.com>
2025-02-05 23:09:04 -0300

    update and delete method - routes and controllers

--- basic-express/src/controllers/livroController.js
+++ basic-express/src/controllers/livroController.js
@@ -13,18 +13,37 @@ class LivroController {
             const livroEncontrado = await livro.findById(id);
             res.status(200).json(livroEncontrado)
         } catch (error) {
-            res.status(500).json({message: `${error.message} - falha ao obter o livro`});
+            res.status(500).json({message: `${error.message} - Falha ao obter o livro`});
         }
     }
 
     static async cadastrarLivro(req, res) {
         try {
             const novoLivro = await livro.create(req.body);
-            res.status(201).json({message: "Adicionado com sucesso", livro: novoLivro})
+            res.status(201).json({message: "Adicionado com sucesso", livro: novoLivro});
         } catch (erro) {
-            res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`});
+            res.status(500).json({message: `${erro.message} - Falha ao cadastrar livro`});
         }
     }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const novoLivro = await livro.findByIdAndUpdate(req.params.id, req.body, {new: true})
+            res.status(200).json({message: "Livro atualizado com sucesso", livro: novoLivro});
+        } catch (error) {
+            res.status(500).json({message: `${error.message} - Falha ao atualizar livro`});
+        }
+    }
+
+    static async deletarLivro(req, res) {
+        try {
+            const livroDeletado = await livro.findByIdAndDelete(req.params.id, req.body, {new: true})
+            res.status(200).json({message: "Livro removido com sucesso", livro: livroDeletado});
+        } catch (error) {
+            res.status(500).json({message: `${error.message} - Falha ao deletar livro`});
+        }
+    }
+
 };
 
 export default LivroController;
