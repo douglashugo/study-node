b3725b4f957b8efe777acd85df3d3eb88b1b34e1
Douglas Hugo <douglas.hugo@live.com>
2025-02-10 22:32:15 -0300

    refatorando manipulador de erros

--- basic-express/src/middlewares/manipuladorDeErros.js
+++ basic-express/src/middlewares/manipuladorDeErros.js
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
     if (erro instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos." });
+        new RequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
-        // if (erro.erros) {
-            const mensagensErros = Object.values(erro.erros)
-        console.log(mensagensErros)
-            .map(erro => erro.message)
-            .join(";");
-        res.status(400).send({ message: `Os seguintes erros foram encontrados: ${mensagensErros}` });
-        // }
+        new ErroValidacao(erro).enviarResposta(res);
     } else {
-        res.status(500).send({ message: "Erro interno do servidor." });
+        new ErroBase().enviarResposta(res);
     }
 }
 
